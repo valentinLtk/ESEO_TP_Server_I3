8bc88930d8babbcdb4087b8e211ca50539b8f1dc
Valentin Latanska <valentinlatanska@172.23.2.77>
2021-11-16 15:37:19 +0100

    First commit

---
+++ src/main/java/com/controller/VilleController.java
@@ -0,0 +1,17 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VilleController {
+	
+	@RequestMapping(value = "/ville", method = RequestMethod.GET)
+	@ResponseBody
+	public String appelGet(@RequestParam(required=false, value="codePostal") String codePostal) {
+		System.out.println("Appel Get");
+		return "Hello";
+	}
+}
